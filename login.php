8411145581dda19047d165f7845f51739b555394
emmysoft123 <47819331+emmysoft123@users.noreply.github.com>
2021-10-11 03:05:43 +0100

    added a login page built with php

---
+++ login.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<!--<![endif]-->
+
+<head>
+    <title>DSS IN AGRICULTURE</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="icon" type="image/png" href="images/classify.JPg">
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+        rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i"
+        rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Droid+Serif:400,400italic" rel="stylesheet" type="text/css">
+    <!--Bootstrap-->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <!--font Awesome-->
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+    <!--slider-->
+    <link href="css/owl.carousel.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <!--PrettyPhoto-->
+    <link href="css/prettyPhoto.css" rel="stylesheet" type="text/css">
+    <!--main file-->
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!--Responsive file-->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <link href="css/sweetalert.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <!-- Preloader -->
+        <div class="preloader"></div>
+        <header>
+            <div class="top_bar">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-7 col-md-7">
+                            <p><i class="fa fa-phone"></i>07083861345</p>
+                            <p><i class="fa fa-envelope"></i>ndubuisichukwuemeka2@gmail.com</p>
+                        </div>
+                        <div class="col-sm-5 col-md-5 text-right">
+
+                            <div class="social_icons">
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-google-plus"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header_bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3 col-md-2">
+                            <div class="logo">
+                                <a href="index-2.html"><img src="images/nclassify.jpg" alt="img" /></a>
+                            </div>
+                        </div>
+                        <div class="col-sm-7 col-md-7 text-right">
+                            <nav>
+                                <ul>
+                                    <li class="dropdown">
+                                        <a href="index-2.html" class=" dropdown-toggle">Home</a>
+
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="about.html" class=" dropdown-toggle">About</a>
+
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="services_detail.php" 
+                                            class=" dropdown-toggle">sign up </a>
+                                    </li>
+                                 
+                                    <li class="dropdown">
+                                        <a href="shop.html">Store</a>
+
+
+
+
+                                    </li>
+
+                                    </li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-sm-9 col-md-2 col-xs-12 responsive_center">
+
+                            <form action="#">
+                                <input type="text" placeholder="search" />
+                                <input type="submit" value="" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+    <!--End Header-->
+    <!--Responsive Nav-->
+    <div class="responsive_button">
+        <p>Home</p>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div class="responsive_nav collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li><a href="index-2.html">Home One</a></li>
+            <li><a href="index2.html">Home Two</a></li>
+            <li><a href="index3.html">Home Three</a></li>
+            <li><a href="index4.html">Home Four</a></li>
+            <li><a href="index5.html">Home Five</a></li>
+            <li><a href="shop.html">Home shop</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="team.html">Team</a></li>
+            <li><a href="team-details.html">Team Details</a></li>
+            <li><a href="testimonials.html">Testimonials</a></li>
+            <li><a href="price.html">Price</a></li>
+            <li><a href="faq.html">FAQ's</a></li>
+            <li><a href="error.html">Error 404</a></li>
+            <li><a href="coming-soon.html">Coming Soon</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="services-2.html">Services style 2</a></li>
+            <li><a href="services_detail.html">Services Detail</a></li>
+            <li><a href="3-column-gallery.html">3 Column Gallery</a></li>
+            <li><a href="full-width-gallery.html">Full width Gallery</a></li>
+            <li><a href="project_detail.html">Gallery Detail</a></li>
+            <li><a href="shop.html">Prducts</a></li>
+            <li><a href="products-details.html">Product Details</a></li>
+
+            <li><a href="checkout.html">Checkout</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="single.html">Blog Detail</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </div>
+    <!--Responsive Nav-->
+    <!--Start Page Title-->
+
+    <!--End Page Title-->
+    <!--Start Services Detail-->
+
+    <!--End Services Detail-->
+    <!--Start Quote-->
+    <div class="qoute_wrap_two seaction_margin animated fades">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-6">
+                    <div class="quote_content">
+                        <div class="inner">
+                            <div class="heading_wrap">
+                                <div class="icon_bar"></div>
+                                <h2 class="heading_a">LOGIN PAGE</h2>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                 <?php 
+            if(isset($_GET['ok']) and $_GET['ok'] == 1): 
+            $output = 'You can now login';
+            include('alert_success.php');
+            endif;
+            ?>
+                <div class="col-sm-6 col-md-6">
+                    <div class="qoute_two">
+                        <form action="includes/process.php" method="post" class="row">
+                            <div class="col-sm-12 col-md-12">
+                                <input type="text" placeholder="username" name="username" class="form-control" />
+                            </div>
+
+                            <div class="col-sm-12 col-md-12">
+                                <input type="password" placeholder="password" name="password" class="form-control" />
+                                <input type ="hidden" name="login" />
+                            </div>
+
+
+                            <div class="col-sm-4 col-md-4">
+                                <button  type="submit" class="button"> LOGIN</button>
+
+                            </div>
+                            <div class="col-sm-8 col-md-8 text-right">
+                                <p>OR Call Us: <strong>07083861345</strong></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm-3 col-md-3"></div>
+            </div>
+        </div>
+    </div>
+    <!--End Quote-->
+    <!--Start Call to Action -->
+
+    <!--End Call to Action -->
+    <!--Start Footer -->
+    <div class="footer_bottom">
+        <div class="footer_inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 col-md-4 animated slide">
+                        <div class="footer_widget">
+                            <a href="#"><img src="images/white_logo.png" alt="img"></a>
+                            <p>Adipisicing elit sed do eiusmod tempor incidunt labore et dolore magna aliqua enim minim
+                                ven quis nostu exercitation ullamco.
+                            </p>
+                            <hr>
+                            <p><i class="fa fa-map-marker"></i>MICHAEL OKPARA UNIVERSITY OF AGRICULTURE,UMUDIKE</p>
+                            <p><i class=" fa fa-phone"></i>Toll Free; 07083861345</p>
+                            <p><i class="fa fa-envelope"></i>ndubuisichukwuemeka2@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-2 col-md-2 animated slide">
+                        <div class="footer_widget">
+                            <h5>Quick Links</h5>
+                            <ul>
+                                <li><a href="index-2.html">Home Page</a></li>
+                                <li><a href="about.html">About us</a></li>
+                                <li><a href="services.html">Our Services</a></li>
+
+
+                                <li><a href="blog.html">Our News</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 col-md-3 animated slide">
+                        <div class="footer_widget">
+                            <h5>Working Hours</h5>
+                            <address>
+                                <p><span>We are open 24/7.</span></p>
+
+                            </address>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-3 col-md-3 animated slide">
+                        <div class="footer_widget">
+                            <h5>Contact Us</h5>
+                            <p>MICHAEL OKPARA UNIVERSITY OF AGRICULTURE,UMUDIKE
+                            </p>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <footer>
+        <div class="container animated slide">
+            <div class="row">
+                <div class="col-sm-8 col-md-8">
+                    <p>Copyrights 2019 <strong>DSS IN AGRICULTURE</strong> • All Rights Reserved.</p>
+                </div>
+                <div class="col-md-4 col-sm-4 text-right">
+                    <div class="social_icon">
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-google-plus"></i></a>
+                        <a href="#"><i class="fa fa-envelope"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <a href="#0" class="cd-top"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Jquery Ui -->
+    <script src="js/jquery-ui.js"></script>
+    <!-- Slider -->
+    <script src="js/owl.carousel.js"></script>
+    <!-- Isotope -->
+    <script src="js/isotope-docs.min.js"></script>
+    <!-- PrettyPhoto -->
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <!-- Parallax -->
+    <script src="js/jquery.parallax.js"></script>
+    <!-- Slider -->
+    <script src="js/jquery.cycle.all.js"></script>
+    <!-- Appear -->
+    <script src="js/jquery.appear.js"></script>
+    <!-- Custom -->
+    <script src="js/custom.js"></script>
+    <script src="js/sweetalert.min.js"></script>
+</body>
+
+</html>
